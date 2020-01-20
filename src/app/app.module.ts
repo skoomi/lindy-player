e9212d025943ee9d1bcf650c6b2fddbe08ef39d7
Michał Skowron <michal.skowron92@gmail.com>
2020-01-20 23:55:21 +0100

    C1 - test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './navbar/navbar.component';
+import { ContentComponent } from './content/content.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
