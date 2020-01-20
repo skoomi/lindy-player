46e807f607db8610c5f5d8d9bf5d925a11ccaa81
Michał Skowron <michal.skowron92@gmail.com>
2020-01-21 00:25:07 +0100

    another attempts to deploy on heroku

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-content',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
 
+
+  spotify() {
+
+  }
 }
