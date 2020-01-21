acf0aa17fa2cbf6bc5b79ec2ffeada41d57735c3
Michał Skowron <michal.skowron92@gmail.com>
2020-01-21 01:14:40 +0100

    zamiana nazwy

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('plejer app is running!');
+    expect(page.getTitleText()).toEqual('lindy-player app is running!');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'plejer'`, () => {
+  it(`should have as title 'lindy-player'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('plejer');
+    expect(app.title).toEqual('lindy-player');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('plejer app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('lindy-player app is running!');
   });
 });
