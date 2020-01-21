9c8e6d90c08ad4ac593a1f18e34c053f24fd4068
Michał Skowron <michal.skowron92@gmail.com>
2020-01-21 01:10:25 +0100

    asd

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/lindy-dj-pro'));
+app.use(express.static('./dist/lindy-player'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/lindy-dj-pro/index.html'));
+res.sendFile(path.join(__dirname,'/dist/lindy-player/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
